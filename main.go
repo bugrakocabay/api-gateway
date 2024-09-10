f90c43a8200e56e77d5a8705909177bf29169939
Bugra Kocabay <kocabaybugra@gmail.com>
2024-09-10 07:54:03 +0300

    feat: init project with basic route configuration

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+const PORT = 8080
+
+type Route struct {
+	Path   string `json:"path"`
+	Target string `json:"target"`
+	Method string `json:"method"`
+}
+
+type Config struct {
+	Routes []Route `json:"routes"`
+}
+
+func loadConfig(path string) (*Config, error) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func createHandler(route Route) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != route.Method {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		target, err := url.Parse(route.Target)
+		if err != nil {
+			http.Error(w, "Invalid Target URL", http.StatusInternalServerError)
+			return
+		}
+
+		proxy := &httputil.ReverseProxy{
+			Director: func(req *http.Request) {
+				req.URL = target
+				req.Host = target.Host
+				req.Method = route.Method
+			},
+		}
+		log.Printf("Proxying request: %s %s -> %s", route.Method, r.URL.Path, target)
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatalf("failed to load configuration: %v", err)
+	}
+
+	for _, route := range config.Routes {
+		http.HandleFunc(route.Path, createHandler(route))
+	}
+
+	log.Printf("Starting API Gateway on PORT: %d", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
+}
